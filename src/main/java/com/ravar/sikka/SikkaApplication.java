7730768aee92b55e07f534cee4af724d927f3f74
Rahul Verma <contactrahul39@gmail.com>
2019-07-21 17:54:48 +0530

    Initial Spring Environment setup

---
+++ src/main/java/com/ravar/sikka/SikkaApplication.java
@@ -0,0 +1,13 @@
+package com.ravar.sikka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SikkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SikkaApplication.class, args);
+	}
+
+}
